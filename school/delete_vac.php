fe9efabf50f45960eccb651d8b5852fbf2cd7a6b
KaelNeutro <32816984+KaelNeutro@users.noreply.github.com>
2018-11-04 23:56:32 -0200

    Update school/delete_vac.php

--- school/delete_vac.php
+++ school/delete_vac.php
@@ -7,12 +7,12 @@
  */
 include("../database/db_conection.php");
 $delete_id=$_GET['del'];
-$delete_query="delete  from students WHERE code='$delete_id'";//delete query
+$delete_query="delete  from vacancies WHERE code='$delete_id'";//delete query
 $run=mysqli_query($dbcon,$delete_query);
 if($run)
 {
 //javascript function to open in the same window
-    echo "<script>window.open('view_std.php? has been deleted','_self')</script>";
+    echo "<script>window.open('view_vac.php? has been deleted','_self')</script>";
 }
 
 ?>
